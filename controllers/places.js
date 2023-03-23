95bbdb3fac4dda9893c361129110fae7d8919fa7
Yash Patel <officialy.yash@gmail.com>
2023-03-23 17:07:04 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,21 +1,7 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/rest-rant-1.jpg'
-      }, {
-          name: 'Coding Cat Cafe',
-          city: 'Phoenix',
-          state: 'AZ',
-          cuisines: 'Coffee, Bakery',
-          pic: '/images/rest-rant-2.jpg'
-      }]
-      
-      
     res.render('places/index', {places})
 })
 
@@ -25,8 +11,20 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places')
-})
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+        // Default city name if one is not provided
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+})  
 
 router.get('/:id', (req, res) => {
     res.send('Show one place in detail (Associated rants, new rant form, delete rant button)')
