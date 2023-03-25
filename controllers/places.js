741d0dfb6b58310960f7bdb91a182ce03891109e
Yash Patel <officialy.yash@gmail.com>
2023-03-25 12:33:16 -0400

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
+
 router.get('/', (req, res) => {
     res.render('places/index', {places})
 })
@@ -35,7 +36,7 @@ router.get('/:id', (req, res) => {
       res.render('error404')
     }
     else {
-      res.render('places/show')
+      res.render('places/show', {places:places[id], id})
     }
 })
   
@@ -48,8 +49,21 @@ router.put('/:id', (req, res) => {
 })
 
 router.delete('/:id', (req, res) => {
-    res.send('Delete a place')
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id, 1)
+      res.redirect('/places')
+    }
 })
+  
+  
+  
 
 router.post('/:id/rant', (req, res) => {
     res.send('Add rant to a place')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,22 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{data.places.id}</h1>
+            <img src={data.places.pic} alt={places.name} />
+            <h1>Currently Unrated</h1>
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+              Edit
+            </a>
+            <form method="POST" action=""> 
+              <button type="submit" className="btn btn-danger">
+                Delete
+              </button>
+            </form>     
+
           </main>
         </Def>
     )
