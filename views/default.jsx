c828a6eca1fdfd9331fc19b48057279108f6c603
Yash Patel <officialy.yash@gmail.com>
2023-03-25 11:40:53 -0400

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -9,7 +9,22 @@ function Def (html) {
                 <link rel="stylesheet" href="/css/style.css"></link>
             </head>
             <body>
+            <nav>
+              <ul>
+                <li>
+                  <a href="/">Home</a>
+                </li>
+                <li>
+                  <a href="/places">Places</a>
+                </li>
+                <li>
+                  <a href="/places/new">Add Place</a>
+                </li>
+              </ul>
+            </nav>
+
                 {html.children}
+                <footer className="footer">Resume | Portfolio | Linkedin </footer>
             </body>
         </html>
     )
